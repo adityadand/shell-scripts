59ac7f603ed0dde877a891e7c399237c29d2362e
aditya dand <33375699+adityadand@users.noreply.github.com>
2019-04-26 13:23:03 +0530

    Add files via upload

---
+++ turnwifion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Purpose :
+#Version: 
+#Created Date: Thu 21 Mar 2019 01:18:58 PM IST
+#Modified Date:
+#Author: f!r3 (10ud
+# START #
+service networking restart
+service network-manager restart
+# END # 
