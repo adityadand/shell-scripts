f6a334de2b8dc831b9454df1f0ee48b012653522
aditya dand <33375699+adityadand@users.noreply.github.com>
2019-04-26 13:24:37 +0530

    Add files via upload

---
+++ aptbasicinstall.sh
@@ -0,0 +1,6 @@
+apt install greed
+apt install cmatrix
+apt install fortune
+apt install cowsay
+apt install sl
+
---
+++ infloop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Purpose : loop bitch
+#Version: 
+#Created Date: Mon Mar 11 22:40:13 IST 2019
+#Modified Date:
+#Author: f!r3 (10ud
+# START #
+echo "enter ur query"
+read query
+while(true)
+do
+$query
+done
+
+
+# END # 
